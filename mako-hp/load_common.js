4de33beea33e95b517faefbf47c00353eb9c8b94
FMmako <104855445+FMmako@users.noreply.github.com>
2025-02-12 16:51:27 +0900

    Delete mako-hp directory

--- mako-hp/load_common.js
+++
@@ -1,9 +0,0 @@
-// ヘッダ
-fetch("/mako-hp/header.html")
-    .then((response) => response.text())
-    .then((data) => document.querySelector("#header").innerHTML = data);
-
-// フッタ
-fetch("/mako-hp/footer.html")
-    .then((response) => response.text())
-    .then((data) => document.querySelector("#footer").innerHTML = data);
